5cf398a6db4d2682ce2738fccbc64e0c06bc88da
Jose Dario Tacacho <tacachojose@gmail.com>
2020-04-21 15:19:26 -0300

    ARREGLO de una etiqueta

--- ecommerce/resources/views/layouts.blade.php
+++ ecommerce/resources/views/layouts.blade.php
@@ -27,14 +27,11 @@
         </nav>
 
     </header>
-
+    <div>
     <main class="container">
         <h1>@yield('h1')</h1>
-
         @yield('main')
-
     </main>
-
     </div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
