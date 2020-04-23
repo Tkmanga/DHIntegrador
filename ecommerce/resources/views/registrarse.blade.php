88c8c9eaebfb50bad93db08f24e9efaa9ca1fd7f
Jose Dario Tacacho <tacachojose@gmail.com>
2020-04-22 21:02:15 -0300

    ARREGLO de una etiqueta

--- ecommerce/resources/views/registrarse.blade.php
+++ ecommerce/resources/views/registrarse.blade.php
@@ -6,7 +6,6 @@
   "\css\backgroundBlue.css"
 @endsection
 @section('main')
-
   		<H1>Abrir una cuenta</H1>
   		<h4>¿Ya eres un usuario ?<a href="login">Inicia sesiòn</a></h4>
   		<br>
@@ -39,21 +38,17 @@
         <div class="">
         <label for="Foto">Foto de perfil en png o jpg </label>
         <form enctype="multipart/form-data" action="uploader.php" method="POST">
-         <input name="uploadedfile" type="file" />
-         <input type="submit" value="submit">
-         	<input type="reset" value="reset">
-
-
-          <div class="clearfixx">
-            <label class="pull-left checkbox-inline"><input type="checkbox" id="Remember" name="Remember"> Remember me</label>
-
-
-  			<p>
-  				Al crear una cuenta aceptas nuestras Condiciones de uso y Aviso de Privacidad.
-  			</p>
-        <br>
-
+             <input name="uploadedfile" type="file" />
+             <input type="submit" value="submit">
+                <input type="reset" value="reset">
+                  <div class="clearfixx">
+                    <label class="pull-left checkbox-inline"><input type="checkbox" id="Remember" name="Remember"> Remember me</label>
+                        <p>
+                            Al crear una cuenta aceptas nuestras Condiciones de uso y Aviso de Privacidad.
+                        </p>
+                  </div>
+                <br>
   		</form>
-    </form>
-  </div>
+        </div>
+        </form>
 @endsection
